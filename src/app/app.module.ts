fedc1ff59fbf2d926b8b5b333a256e0929859054
Peanuts-83 <tranque@free.fr>
2022-06-22 21:00:38 +0200

    init commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { NouveauComponent } from './nouveau/nouveau.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NouveauComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
